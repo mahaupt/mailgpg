1e1864f4e01ec0e37e0497e1d89d9d7bcbac3058
mahaupt <subcortexx@gmail.com>
2026-03-17 14:44:13 +0100

    Initial Commit

---
+++ MailGPG/MailGPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailGPGApp.swift
+//  MailGPG
+//
+//  Created by Marcel Haupt on 17.03.26.
+//
+
+import SwiftUI
+
+@main
+struct MailGPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
